9f467422ba1942d474428c5c00a771230249ebde
Meds Pogi <69465320+MedsAsperin@users.noreply.github.com>
2023-03-11 16:11:30 +0800

    Uploaded layout and Navbar component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
+<script>
+
+	import { each } from "svelte/internal";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <h1>real_sirlije</h1>
+
+{#each Array(3) as _, i}
+    <li>{i + 1}</li>
+{/each}
+
